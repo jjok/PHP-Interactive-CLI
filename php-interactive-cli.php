cca2dde7bce8c93310ecb5eff13703f37047d845
Jonathan Jefferies <jonathan@jjok.co.uk>
2011-05-18 23:55:50 +0100

    Added main class file.

---
+++ php-interactive-cli.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class InteractiveCLI {
+	protected $goodbye = "bye\n";
+
+	public function __construct($input) {
+		#echo '>>';
+		#do while
+		while($line = trim(fgets($input))) {
+			$this->readLine($line);
+		}
+		if(!is_null($this->goodbye)) echo $this->goodbye;
+	}
+
+	abstract protected function readLine($command);
+}
