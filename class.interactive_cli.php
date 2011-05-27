62b7a0c60cf6f2ecad68ca40dc41bda3339cd019
Jonathan Jefferies <jonathan@jjok.co.uk>
2011-05-27 13:06:49 +0100

    Added tracking of command history. Added checking of STDIN. Removed default messages and prompt.
    Change-Id: I6341a4cf947279593507666b4aebb8be913a2614

--- class.interactive_cli.php
+++ class.interactive_cli.php
@@ -6,36 +6,67 @@
  *
  */
 abstract class InteractiveCLI {
-	protected $welcome = 'Welcome!';
-	protected $prompt = '>> ';
-	protected $goodbye = 'Bye!';
-	protected $exit = 'exit';
+	private $history = array();
+
+	protected $welcome = '';
+	protected $prompt = '';
+	protected $goodbye = '';
+	protected $exit = '';
+	#protected $help = 'Type "exit" to exit.';
 
 	public function __construct() {
-		echo $this->welcome;
-
-		while(true) {
-			echo "\n$this->prompt";
-			$line = trim(fgets(STDIN));
-
-			switch($line) {
-				case $this->exit:
-					break 2;
-				case '':
-					continue 2;
-				default:
-					try {
-						$this->readLine($line);
-					}
-					catch(Exception $e) {
-						$this->handleError($e);
-					}
+		if(defined('STDIN')) {
+			echo $this->welcome;
+	
+			while(true) {
+				if($this->prompt != '') {
+					echo "\n$this->prompt";
+				}
+				$line = trim(fgets(STDIN));
+	
+				switch($line) {
+					case $this->exit:
+						break 2;
+					case '':
+						#echo $this->help;
+						continue 2;
+					default:
+						try {
+							$this->readLine($line);
+						}
+						catch(Exception $e) {
+							$this->handleError($e);
+						}
+				}
 			}
+			echo $this->goodbye;
 		}
-		echo $this->goodbye;
+		else throw new Exception('This program runs for the command line.');
 	}
 
 	abstract protected function readLine($command);
+
+	/**
+	 * 
+	 * Enter description here ...
+	 * @param string $command
+	 */
+	protected function addToHistory($command) {
+		$this->history[] = $command;
+	}
+
+	protected function getHistory() {
+		return $this->history;
+	}
+
+	/**
+	 * 
+	 * Clear the command history
+	 * @return void
+	 */
+	protected function clearHistory() {
+		$this->history = array();
+	}
 
 	protected function handleError(Exception $e) {
 		echo $e->getMessage();
